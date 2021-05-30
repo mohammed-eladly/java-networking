831399584a32154cc6be8e0b0c3ce3fcd52e757d
mohammed-eladly-afaqy <mohammed.eladly@afaqy.com>
2021-05-30 12:37:45 +0200

    init

---
+++ src/main/java/com/afaqy/demo/network/Main.java
@@ -0,0 +1,20 @@
+package com.afaqy.demo.network;
+
+/**
+ * Name : Main
+ * <p>
+ * Description :
+ * <p>
+ * Date : 30/05/2021
+ * <p>
+ * Create by : Mohammed ElAdly
+ * <p>
+ * Mail : mohammed.eladly@afaqy.com
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hiii");
+    }
+
+}
